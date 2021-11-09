40710c0ab9a347be7510dc05180ac951eaae39db
khamis <mohamed.khamis@tactful.ai>
2021-11-09 23:41:24 +0200

    req and setup

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+# get parent dir
+home="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../ && pwd )/test"
+
+# delete Python virtual environment and recreate it
+rm -rf $home/test/env
+
+virtualenv --python=python3.6 $home/env
+
+# enter the venv and install dependencies
+source $home/env/bin/activate
+
+pip install -r $home/requirements.txt
